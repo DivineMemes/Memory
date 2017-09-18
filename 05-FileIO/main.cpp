39e5564e0387222dc23b053c26431724733ef566
Wyatt <s179053@students.aie.edu.au>
2017-09-18 10:11:43 -0700

    Added Load Entity Data

--- 05-FileIO/main.cpp
+++ 05-FileIO/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+#include <time.h>
+#include "entity.h"
 
 using std::fstream;
 
@@ -11,6 +14,7 @@ using std::endl;
 
 int main()
 {
+	srand(NULL);
 	//tutorial
 	/*
 	fstream file;
@@ -50,6 +54,7 @@ int main()
 	}
 	memes.close();
 	*/
+
 	//1 digital printer
 	/*
 	bool reset = false;
@@ -79,8 +84,8 @@ int main()
 	}
 	*/
 
-
 	//2 my information
+	/*
 	fstream info;
 	string fileName = {};
 	string name = {};
@@ -111,5 +116,76 @@ int main()
 	cout << "File created thanks for providing the nsa with your info! " << endl;
 
 	info.close();
+	*/
+
+	//open
+
+	//running out of toner
+	/*
+	fstream file;
+	string fileName = {};
+	string buffer;
+	cout << "choose an existing file." << endl;
+	cin >> fileName;
+	file.open(fileName, std::ios_base::_Nocreate);
+
+	if (file.fail())
+	{
+		cout << "cannot execute file operation." << endl;
+	}
+
+	while (std::getline(file, buffer))
+	{
+		char garble = { '#' };
+		int randomChance = rand() % 2 + 1;
+		if (randomChance == 1)
+		{
+			std::cout << buffer << std::endl;
+		}
+
+	}
+
+	file.clear(); //resetting error flags
+	file.close();
+	*/
+
+fstream entityStream;
+entityStream.open("monsters.txt");
+if (entityStream.fail())
+{
+	cout << "Error" << endl;
+}
+int entityCount = 0;
+Entity entities[100];
+
+while (true)
+{
+	std::string buf;
+	bool entFound = false;
+	while (getline(entityStream, buf))
+	{
+		if (buf[0] == '@') { break; }
+		entFound = true;
+		break;
+	}
+	if (!entFound) { break; }
+	Entity& curEntity = entities[entityCount];
+	getline(entityStream, buf);
+	curEntity.hitpoints = stof(buf);
+	getline(entityStream, buf);
+	curEntity.armor = stof(buf);
+
+	getline(entityStream, buf);
+	curEntity.strength = stof(buf);
+	getline(entityStream, buf);
+	curEntity.defense = stof(buf);
+	getline(entityStream, buf);
+	curEntity.agility = stof(buf);
+	getline(entityStream, buf);
+	curEntity.luck = stof(buf);
+	entityCount++;
+	if (entityCount >= 100) { break; }
+}
+
 	while (true) {};
 }
